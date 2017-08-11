c6591169162cea71cb6440d3e583aec7ddbd7824
kylel <kyle.ludwig@gmail.com>
2017-08-11 14:17:29 +0200

    added all c keywords

--- jsc.js
+++ jsc.js
@@ -9,7 +9,14 @@ Jsc = function(input, output, vars, error) {
 	this.tokens = [];
 }
 
-var cKeyWords = ['if', 'for', 'continue', 'while', 'break', 'switch', 'case', 'goto', 'default', 'do', 'return'];
+var cKeyWords = [	'auto', 'break', 'case', 'char',
+					'const', 'continue', 'default', 'do',
+					'double', 'else', 'enum', 'extern', 
+					'float', 'for', 'goto', 'if',
+					'int', 'long', 'register', 'return'
+					'short', 'signed', 'sizeof', 'static',
+					'struct', 'switch', 'typedef', 'union',
+					'unsigned', 'void', 'volatile', 'while'];
 
 Jsc.prototype.main = function() {
 	var tokens = this.lex();
